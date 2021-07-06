65c029750649858887e2c86214c3e3dc3bd744b0
Prasundas99 <prasundas115@gmail.com>
2021-07-06 14:31:26 +0530

    [new] api to fetch single instrument

--- server/routes/instruments.js
+++ server/routes/instruments.js
@@ -4,11 +4,14 @@ import {
   createInstrument,
   updateInstrument,
   deleteInstrument,
+  getSingleInstrument
 } from "../controllers/instruments.controller.js";
 
 const router = express.Router();
 
 router.get("/", getInstrument);
+router.get("/:id",getSingleInstrument);
+
 router.post("/add", createInstrument);
 router.put("/:id", updateInstrument);
 router.delete("/:id", deleteInstrument);
