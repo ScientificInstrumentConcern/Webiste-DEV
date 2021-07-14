93c45731246cbb0b2bfa460c01dacabdf44d7225
Prasundas99 <prasundas115@gmail.com>
2021-07-15 00:42:49 +0530

    Carousal pic added

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
-
+import { Container } from '@material-ui/core';
 import Hero from './components/Hero';
-import { useStyles } from '../../Styles/Home/heroStyling';
 import Products from './components/Products';
 import About from './components/About';
-import { Container } from '@material-ui/core';
+import Clients from './components/Clients';
+import { useStyles } from '../../Styles/Home/heroStyling';
 
 function Home() {
     const classes = useStyles();
 
     return (
-        <div>
+        <div className={classes.root}>
             <Hero />
             <Container>
                 <About />
                 <Products />
+                <Clients />
             </Container>
         </div>
     );
