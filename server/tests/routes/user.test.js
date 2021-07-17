80799ebb64cd129ec8eba8b3952dad42bc93a1c1
Prasundas99 <prasundas115@gmail.com>
2021-07-17 16:15:52 +0530

    ran failed jest test

--- server/tests/routes/user.test.js
+++ server/tests/routes/user.test.js
@@ -1,7 +1,12 @@
+import request from 'supertest';
 import supertest from 'supertest';
 import userRouter from '../../routes/user.js'
 
 describe('GET /admin/', function(){
    //Get http status  200
+   test("Should respond with a 200 status code", () => {
+       const response = await request(userRouter).get("/admin");
+       expect(response.statusCode).toBe(200)
+   })
    //get json data
 })
