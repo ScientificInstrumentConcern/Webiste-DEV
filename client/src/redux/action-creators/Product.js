3aea6844250734649638a54ad328ec840e00cfab
Prasundas99 <prasundas115@gmail.com>
2021-07-23 13:00:12 +0530

    Product action done

--- client/src/redux/action-creators/Product.js
+++ client/src/redux/action-creators/Product.js
@@ -1,1 +1,61 @@
-import {} from '../api/product'
+import { getProduct, getSingleProduct } from '../api/product';
+import {
+    FETCH_PRODUCT,
+    FETCH_PRODUCT_FAILED,
+    FETCH_SINGLE_PRODUCT_FAILED,
+    FETCH_PRODUCT_REQUEST,
+    FETCH_SINGLE_PRODUCT_REQUEST,
+} from '../action-constants';
+
+
+/**
+ * @description: Views all the product from database
+ */
+ export const fetchProduct = () => async (dispatch) => {
+    dispatch({
+        type: FETCH_PRODUCT_REQUEST,
+    });
+    try {
+        const { data } = await getProduct();
+        dispatch({
+            type: FETCH_PRODUCT,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: FETCH_PRODUCT_FAILED,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+/**
+ *
+ * @description: Views single the product from database
+ * @param {string} id
+ *
+ */
+ export const fetchSingleProduct = (id) => async (dispatch) => {
+    try {
+        dispatch({
+            type: FETCH_SINGLE_PRODUCT_REQUEST,
+        });
+
+        const { data } = await getSingleProduct(id);
+        dispatch({
+            type: FETCH_SINGLE_PRODUCT,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: FETCH_SINGLE_PRODUCT_FAILED,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
